af51fbaf41d27f0797dad18b38622e58ecf19dce
Rashedul Hasan <rashedulhasansojib@gmail.com>
2024-10-13 14:08:12 +0600

    basic frontend structure added (props changed)

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,19 +1,27 @@
-import React from "react";
-import "./UsersList.css";
-import "./UserItem";
-import UserItem from "./UserItem";
-const UsersList = ({ users }) => {
-  if (!users.length) {
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
+  if (props.items.length === 0) {
     return (
       <div className="center">
         <h2>No users found.</h2>
       </div>
     );
   }
+
   return (
     <ul className="users-list">
-      {users.map((user) => (
-        <UserItem key={user.id} {...user} />
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
       ))}
     </ul>
   );
